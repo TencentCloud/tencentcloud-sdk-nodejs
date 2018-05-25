2b9e74c36e0b21051c87039b8e8b3291dc0bf5c2
coolli(李帅) <coolli@tencent.com>
2018-05-25 14:10:26 +0800

    release_3.0.1

--- tencentcloud/index.js
+++ tencentcloud/index.js
@@ -1,8 +1,18 @@
 module.exports = {
+    aai: require("./aai"),
+    batch: require("./batch"),
     common: require("./common"),
     cvm: require("./cvm"),
     cbs: require("./cbs"),
     cdb: require("./cdb"),
-    es: require("./es"),
+    cws: require("./cws"),
+    dcdb: require("./dcdb"),
+    iot: require("./iot"),
+    mariadb: require("./mariadb"),
+    partners: require("./partners"),
+    postgres: require("./postgres"),
+    scf: require("./scf"),
+    tmt: require("./tmt"),
+    vpc: require("./vpc"),
 };
 
