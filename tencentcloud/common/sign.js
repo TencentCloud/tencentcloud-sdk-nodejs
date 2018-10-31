1afea2415603f0d805af6347fe7758b02bf6b93d
Weijia Wang <starkwang@126.com>
2018-10-31 11:55:27 +0800

    实现3.0签名

--- tencentcloud/common/sign.js
+++ tencentcloud/common/sign.js
@@ -18,5 +18,99 @@ class Sign {
         let hmac = crypto.createHmac(signMethodMap[signMethod], secretKey || "");
         return hmac.update(Buffer.from(signStr, 'utf8')).digest('base64')
     }
+
+    static sign3({
+        method = 'POST',
+        url = '',
+        payload,
+        timestamp,
+        service,
+        secretId,
+        secretKey,
+        multipart,
+        boundary
+    }) {
+        const urlObj = new URL(url)
+    
+        // 通用头部
+        let headers = ''
+        let signedHeaders = ''
+        if (method === 'GET') {
+            signedHeaders = 'content-type'
+            headers = 'content-type:application/x-www-form-urlencoded\n'
+        } else if (method === 'POST') {
+            signedHeaders = 'content-type'
+            if (multipart) {
+                headers = `content-type:multipart/form-data; boundary=${boundary}\n`
+            } else {
+                headers = 'content-type:application/json\n'
+            }
+        }
+        headers += `host:${urlObj.hostname}\n`
+        signedHeaders += ';host'
+    
+        const path = urlObj.pathname
+        const querystring = urlObj.search.slice(1)
+    
+        let payload_hash = ''
+        if (multipart) {
+            const hash = crypto.createHash('sha256')
+            hash.update(`--${boundary}`)
+            for (let key in payload) {
+                const content = payload[key]
+                if (Buffer.isBuffer(content)) {
+                    hash.update(`\r\nContent-Disposition: form-data; name="${key}"\r\nContent-Type: application/octet-stream\r\n\r\n`)
+                    hash.update(content)
+                    hash.update('\r\n')
+                } else if (typeof content === 'string') {
+                    hash.update(`\r\nContent-Disposition: form-data; name="${key}"\r\n\r\n`)
+                    hash.update(`${content}\r\n`)
+                }
+                hash.update(`--${boundary}`)
+            }
+            hash.update(`--\r\n`)
+            payload_hash = hash.digest('hex')
+        } else {
+            payload_hash = payload ? getHash(JSON.stringify(payload)) : getHash('')
+        }
+    
+        const canonicalRequest =
+            method + '\n' +
+            path + '\n' +
+            querystring + '\n' +
+            headers + '\n' +
+            signedHeaders + '\n' +
+            payload_hash
+        const date = getDate()
+    
+        const StringToSign =
+            'TC3-HMAC-SHA256' + '\n' +
+            timestamp + '\n' +
+            `${date}/${service}/tc3_request` + '\n' +
+            getHash(canonicalRequest)
+    
+        const kDate = sha256(date, 'TC3' + secretKey)
+        const kService = sha256(service, kDate)
+        const kSigning = sha256('tc3_request', kService)
+        const signature = sha256(StringToSign, kSigning, 'hex')
+    
+        return `TC3-HMAC-SHA256 Credential=${secretId}/${date}/${service}/tc3_request, SignedHeaders=${signedHeaders}, Signature=${signature}`
+    }
 }
+
+function sha256(message, secret = '', encoding) {
+    const hmac = crypto.createHmac('sha256', secret)
+    return hmac.update(message).digest(encoding)
+}
+
+function getHash(message, encoding = 'hex') {
+    const hash = crypto.createHash('sha256')
+    return hash.update(message).digest(encoding)
+}
+
+function getDate() {
+    const date = new Date()
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+}
+
 module.exports = Sign;
