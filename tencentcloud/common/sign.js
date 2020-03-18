9b335bc920bb31bb9d71b52d433308de17dd766f
vkyin <86160576@qq.com>
2020-03-18 12:11:36 +0800

    Update sign.js
    https://github.com/TencentCloud/tencentcloud-sdk-nodejs/issues/33

--- tencentcloud/common/sign.js
+++ tencentcloud/common/sign.js
@@ -111,9 +111,9 @@ function getHash(message, encoding = 'hex') {
 
 function getDate(timestamp) {
     const date = new Date(timestamp * 1000)
-    const year = date.getFullYear()
-    const month = ('0' + (date.getMonth() + 1)).slice(-2)
-    const day = ('0' + date.getDate()).slice(-2)
+    const year = date.getUTCFullYear()
+    const month = ('0' + (date.getUTCMonth() + 1)).slice(-2)
+    const day = ('0' + date.getUTCDate()).slice(-2)
     return `${year}-${month}-${day}`
 }
 
