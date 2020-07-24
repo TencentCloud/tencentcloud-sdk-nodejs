9d417418e1b0350430cd85984489854b95f820d1
lylianchen <lylianchen@tencent.com>
2020-07-24 14:37:22 +0800

    fix: es6 syn

--- tencentcloud/common/http/fetch.js
+++ tencentcloud/common/http/fetch.js
@@ -2,9 +2,7 @@ const fetch = require('node-fetch');
 const HttpsProxyAgent = require('https-proxy-agent');
 
 module.exports = (url, options) => {
-  const instanceOptions = {
-    ...options
-  };
+  const instanceOptions = options || {};
 
   if (!options.agent && process.env.http_proxy) {
     instanceOptions.agent = new HttpsProxyAgent(process.env.http_proxy);
