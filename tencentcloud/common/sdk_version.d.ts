c7cedd700589eee7aad2dc6ab1311e8d85b742b5
tencentcloudapi <tencentcloudapi@tencent.com>
2024-01-03 15:14:17 +0800

    release 4.0.772

--- tencentcloud/common/sdk_version.d.ts
+++ tencentcloud/common/sdk_version.d.ts
@@ -1,1 +1,1 @@
-export declare const sdkVersion = "4.0.771";
+export declare const sdkVersion = "4.0.772";
--- tencentcloud/common/sdk_version.js
+++ tencentcloud/common/sdk_version.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sdkVersion = void 0;
-exports.sdkVersion = "4.0.771";
+exports.sdkVersion = "4.0.772";
