0435e419b88bd1fad856105d5f4451281329a250
larrytzhang <larrytzhang@tencent.com>
2024-04-22 20:18:54 +0800

    feat: update hunyuan example

--- examples/hunyuan/v20230901/chat_std.ts
+++ examples/hunyuan/v20230901/chat_std.ts
@@ -1,6 +1,6 @@
 import * as tencentcloud from "../../../../tencentcloud-sdk-nodejs"
 import { SSEResponseModel } from "../../../tencentcloud/common/sse_response_model"
-import { ChatProResponse } from "../../../tencentcloud/services/hunyuan/v20230901/hunyuan_models"
+import { ChatStdResponse } from "../../../tencentcloud/services/hunyuan/v20230901/hunyuan_models"
 
 // 导入对应产品模块的client models
 const HunyuanClient = tencentcloud.hunyuan.v20230901.Client
@@ -20,12 +20,13 @@ const client = new HunyuanClient({
 })
 
 // 通过client对象调用想要访问的接口，需要传入请求对象以及响应回调函数
-
+// 流式调用
 client.ChatStd({
   Messages: [{
     Role: 'user',
     Content: '你好, 可以讲个笑话吗'
-  }]
+  }],
+  Stream: true, // 流式调用开关，默认为 true
 }).then(
   async (res) => {
     res.on('message', (message) => {
@@ -35,6 +36,22 @@ client.ChatStd({
     for await (let message of res) {
       console.log(message)
     }
+  },
+  (err) => {
+    console.error('error', err)
+  }
+)
+
+// 非流式调用
+client.ChatStd({
+  Messages: [{
+    Role: 'user',
+    Content: '你好, 可以讲个笑话吗'
+  }],
+  Stream: false
+}).then(
+  async (res) => {
+    console.log(res)
   },
   (err) => {
     console.error('error', err)
